435e694d9f956cfd984c32acaa7083d24faa3173
lydos <lydossky@gmail.com>
2024-11-06 14:12:15 +0300

    initial setup

---
+++ hashMap.js
@@ -0,0 +1,14 @@
+const HashMap = function () {
+  function hash(key) {}
+  function set(key, value) {}
+  function get(key) {}
+  function has(key) {}
+  function remove(key) {}
+  function length() {}
+  function clear() {}
+  function keys() {}
+  function values() {}
+  function entries() {}
+};
+
+export default HashMap;
